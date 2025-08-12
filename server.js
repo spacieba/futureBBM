e457a71e833d1a905ae6b675e2f12d71fbd49852
spacieba <kholotrash@gmail.com>
2025-08-12 22:32:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Database = require('better-sqlite3');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs'); 
 
 const app = express();
 const port = process.env.PORT || 3000;
